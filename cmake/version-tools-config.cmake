5c21687d2744c7888f60d6c67812002fc60a4845
dhdhxji <keysindie@gmail.com>
2025-06-21 14:50:49 +0300

    Add some templating stuff

--- cmake/version-tools-config.cmake
+++ cmake/version-tools-config.cmake
@@ -1,9 +1,14 @@
-function(template_version)
+function(template_version VERSION_FILE TEMPLATE OUTPUT)
 
     add_custom_command(
-        OUTPUT version.h
-        COMMAND python3 ${CMAKE_CURRENT_LIST_DIR}/tools/version_tools.py
-        DEPENDS ${CMAKE_SOURCE_DIR}/version.json ${CMAKE_SOURCE_DIR}/.git/HEAD
+        OUTPUT ${OUTPUT}
+        COMMAND python3 ${CMAKE_CURRENT_LIST_DIR}/tools/version_tools.py 
+                        ${VERSION_FILE}
+                        ${TEMPLATE}
+                        ${OUTPUT}
+        MAIN_DEPENDENCY ${VERSION_FILE}
+        DEPENDS ${CMAKE_SOURCE_DIR}/.git/HEAD
     )
 
+        # WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
 endfunction()
--- tools/version_tools.py
+++ tools/version_tools.py
@@ -1,12 +1,29 @@
 import argparse as ap
+import json
+from dataclasses import dataclass, asdict
+from string import Template
+
+@dataclass
+class Version:
+    major: int
+    minor: int
+    patch: int
 
 arg_parser = ap.ArgumentParser()
-arg_parser.add_argument('--version_file', type=str, help='version.json file location')
+arg_parser.add_argument('version_file', type=str, help='version.json file location')
+arg_parser.add_argument('template', type=str, help='version.json file location')
+arg_parser.add_argument('output', type=str, help='version.json file location')
 
 args = arg_parser.parse_args()
 
 with open(args.version_file) as f:
-    print(f.read())
+    version = json.load(f)
+version = Version(**version)
 
-with open('version.h', 'w') as f:
-    f.write('#define VERSION "1.2.3.4"\n')
+with open(args.template) as f:
+    template = f.read()
+template = Template(template)
+
+
+with open(args.output, 'w') as f:
+    f.write(template.substitute(asdict(version)))
